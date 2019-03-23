1f37c4e0efdae2d19d674115e91c5b77ab3b5483
Ruben Naranjo <rubendario2110@hotmail.com>
2019-03-23 17:48:52 -0500

    delete node modules

---
+++ models/getimageforid.js
@@ -0,0 +1,15 @@
+let collection =require('../models/connection');
+exports.GetImage=function(id){
+  return new Promise(function(resolve,reject){
+    collection.find({}).toArray().then(data => {
+     if (err){
+       console.error(err);
+     }
+      console.log("Found the following records");
+      console.log(data)
+      resolve(data);
+   
+    });
+  })
+ 
+}
