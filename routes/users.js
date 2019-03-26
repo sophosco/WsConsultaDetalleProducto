a322fea3e8fe6f03b3fff15287dcca729169f7f0
Cristian David Dallos Bustos <cristian.dallos@sophos.col.com>
2019-03-26 11:27:06 -0500

    Change GetCatalog to Post

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ var router = express.Router();
 let getCatalog = require('../Controllers/GetCatalog');
 let getProduct = require('../Controllers/GetProduct');
 let reserveProducts = require('../Controllers/ReserveProducts');
-router.get('/products/catalog/', getCatalog.GetCatalog);
-router.get('/products/product/', getProduct.GetProduct);
+router.post('/products/catalog/', getCatalog.GetCatalog);
+router.post('/products/product/', getProduct.GetProduct);
 router.post('/products/reserve/', reserveProducts.ReserveProducts);
 
 module.exports = router;
