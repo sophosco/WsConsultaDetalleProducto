64c00027c555470fa70ca69d8e5318bdafc2f04e
Cristian David Dallos Bustos <cristian.dallos@sophos.col.com>
2019-03-27 20:59:26 -0500

    AB#1377 Bug 01 Active https protocol.

--- services/Security_Service.js
+++ services/Security_Service.js
@@ -3,7 +3,7 @@ let Request = require("request");
 exports.VerifyJwtToken = function (requestVerifyJwtToken, cb) {
     Request.post({
         "headers": { "content-type": "application/json" },
-        "url": "https://localhost:9443/services/security/verifyJwtToken/",
+        "url": "https://localhost:9443/services/security/verifyJwtToken/",        
         "body": JSON.stringify(requestVerifyJwtToken) 
     }, (error, response, body) => {
         if (error) {
