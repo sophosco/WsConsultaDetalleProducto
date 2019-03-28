68f02dc1893070ed5f3ba9f546bdf7e2c15757d4
Cristian David Dallos Bustos <cristian.dallos@sophos.col.com>
2019-03-28 12:10:08 -0500

    AB#1377 Integración funcionalidad de filtros

--- Controllers/GetProduct.js
+++ Controllers/GetProduct.js
@@ -4,20 +4,6 @@ let mongoDB = require('../database/MongoDB');
 
 exports.GetProduct = function (req, res) {
 
-    let productRequest = {
-        "id": req.body.requestPayload.id
-    };
-
-    let token = req.header("X-Session");
-    let id = req.header("X-Channel");
-
-    if(token == undefined){
-        token = req.body.requestHeader.session;        
-    }
-    if(id == undefined){
-        id = req.body.requestHeader.channel;
-    }
-
     let response = {
         "responseHeader": {
             "responseInfo": {
@@ -35,73 +21,93 @@ exports.GetProduct = function (req, res) {
         }
     };
 
-    getSecurityManager.GetVerifyJwtToken(token, id, function (error, responseVerifyJwtToken) {
-        if (error != null) {
-            response.responseHeader.status.code = 500;
-            response.responseHeader.status.description = error;
-            response.responsePayload.result = false;
-            res.status(500).json(response);
-        } else {
-            if (responseVerifyJwtToken.responseHeader.status.code == "01") {
-                response.responseHeader.status.code = 401;
-                response.responseHeader.status.description = responseVerifyJwtToken.responseHeader.status.description;
-                response.responsePayload.result = false;
-                res.status(401).json(response);
-            } else {
-                getProductManager.GetProductManager(productRequest, function (error, product) {
-                    if (error != null) {
-                        response.responseHeader.status.code = 500;
-                        response.responseHeader.status.description = error;
-                        response.responsePayload.result = false;
-                        res.status(500).json(response);
-                    } else {
-                        var collection = "ProductoDetalle";
-                        var filter = { idProducto: product.id };
-                        mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
-                            if (error != null) {
-                                response.responseHeader.status.code = 500;
-                                response.responseHeader.status.description = error;
-                                response.responsePayload.result = false;
-                                res.status(500).json(response);
-                            } else {
-                                product.ratingsValue = result[0].calificacion;
-                                product.name = result[0].informacionAdicional;
-                                product.discount = result[0].descuento;
-                                product.categoryId = result[0].categoria;
-                                collection = "Galeria";
-                                mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
-                                    if (error != null) {
-                                        response.responseHeader.status.code = 500;
-                                        response.responseHeader.status.description = error;
-                                        response.responsePayload.result = false;
-                                        res.status(500).json(response);
-                                    } else {                                        
-                                        response.responseHeader.status.code = 200;
-                                        response.responseHeader.status.description = "Transacción exitosa";
-                                        response.responsePayload.result = true;
-                                        for (var i = 0; i < result.length; i++) {
-                                            product.images.push(
-                                                {
-                                                    "small": result[i].imagenPequena,
-                                                    "medium": result[i].imagenMediana,
-                                                    "big": result[i].imagenGrande
-                                                }
-                                            );
-                                        }
-                                        response.responsePayload.product = product;
-                                        res.setHeader(
-                                            "Access-Control-Allow-Origin", "*",
-                                            "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
-                                        );
-                                        res.status(200).json(response);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        }
-    });
+    try {
+        
+        let productRequest = {
+            "id": req.body.requestPayload.id
+        };
+        let token = req.header("X-Session");
+        let id = req.header("X-Channel");
 
+        if (token == undefined) {
+            token = req.body.requestHeader.session;
+        }
+        if (id == undefined) {
+            id = req.body.requestHeader.channel;
+        }
+
+        getSecurityManager.GetVerifyJwtToken(token, id, function (error, responseVerifyJwtToken) {
+            if (error != null) {
+                response.responseHeader.status.code = 500;
+                response.responseHeader.status.description = error;
+                response.responsePayload.result = false;
+                res.status(500).json(response);
+            } else {
+                if (responseVerifyJwtToken.responseHeader.status.code == "01") {
+                    response.responseHeader.status.code = 401;
+                    response.responseHeader.status.description = responseVerifyJwtToken.responseHeader.status.description;
+                    response.responsePayload.result = false;
+                    res.status(401).json(response);
+                } else {
+                    getProductManager.GetProductManager(productRequest, function (error, product) {
+                        if (error != null) {
+                            response.responseHeader.status.code = 500;
+                            response.responseHeader.status.description = error;
+                            response.responsePayload.result = false;
+                            res.status(500).json(response);
+                        } else {
+                            var collection = "ProductoDetalle";
+                            var filter = { idProducto: product.id };
+                            mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
+                                if (error != null) {
+                                    response.responseHeader.status.code = 500;
+                                    response.responseHeader.status.description = error;
+                                    response.responsePayload.result = false;
+                                    res.status(500).json(response);
+                                } else {
+                                    product.ratingsValue = result[0].calificacion;
+                                    product.name = result[0].informacionAdicional;
+                                    product.discount = result[0].descuento;
+                                    product.categoryId = result[0].categoria;
+                                    collection = "Galeria";
+                                    mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
+                                        if (error != null) {
+                                            response.responseHeader.status.code = 500;
+                                            response.responseHeader.status.description = error;
+                                            response.responsePayload.result = false;
+                                            res.status(500).json(response);
+                                        } else {
+                                            response.responseHeader.status.code = 200;
+                                            response.responseHeader.status.description = "Transacción exitosa";
+                                            response.responsePayload.result = true;
+                                            for (var i = 0; i < result.length; i++) {
+                                                product.images.push(
+                                                    {
+                                                        "small": result[i].imagenPequena,
+                                                        "medium": result[i].imagenMediana,
+                                                        "big": result[i].imagenGrande
+                                                    }
+                                                );
+                                            }
+                                            response.responsePayload.product = product;
+                                            res.setHeader(
+                                                "Access-Control-Allow-Origin", "*",
+                                                "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+                                            );
+                                            res.status(200).json(response);
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    } catch (error) {
+        response.responseHeader.status.code = 500;
+        response.responseHeader.status.description = "Error: " + error;
+        response.responsePayload.result = false;
+        res.status(500).json(response);
+    }
 }
