76c1a69f819a7e6ab174e092c6b59a5480dfc32f
Cristian David Dallos Bustos <cristian.dallos@sophos.col.com>
2019-03-28 19:23:11 -0500

    AB#1377 Integración funcionalidad de filtros v5

--- Controllers/GetProduct.js
+++ Controllers/GetProduct.js
@@ -1,6 +1,7 @@
 let getProductManager = require('../managers/GetProduct_Manager');
 let getSecurityManager = require('../managers/Security_Manager');
 let mongoDB = require('../database/MongoDB');
+let cache = require('memory-cache');
 
 exports.GetProduct = function (req, res) {
 
@@ -49,99 +50,112 @@ exports.GetProduct = function (req, res) {
                     response.responsePayload.result = false;
                     res.status(401).json(response);
                 } else {
-                    getProductManager.GetProductManager(productRequest, function (error, product) {
-                        if (error != null) {
-                            response.responseHeader.status.code = 500;
-                            response.responseHeader.status.description = error;
-                            response.responsePayload.result = false;
-                            res.status(500).json(response);
-                        } else {
-                            var collection = "ProductoDetalle";
-                            var filter = { idProducto: product.id };
-                            mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
-                                if (error != null) {
-                                    response.responseHeader.status.code = 500;
-                                    response.responseHeader.status.description = error;
-                                    response.responsePayload.result = false;
-                                    res.status(500).json(response);
-                                } else {
-                                    product.ratingsValue = result[0].calificacion;
-                                    product.ratingsCount = (Math.floor(Math.random() * (5 - 1)) + 1);
-                                    product.name = result[0].informacionAdicional;
-                                    product.discount = result[0].descuento;
-                                    if (parseInt(result[0].descuento) > 0) {
-                                        product.newPrice = parseInt(product.oldPrice) - ((parseInt(result[0].descuento) * parseInt(product.oldPrice)) / 100);
+                    if (cache.get('product' + req.body.requestPayload.id) != null) {                        
+                        response.responseHeader.status.code = 200;
+                        response.responseHeader.status.description = "Transacción exitosa";
+                        response.responsePayload.result = true;
+                        response.responsePayload.products = cache.get('product' + productRequest.id);
+                        res.setHeader(
+                            "Access-Control-Allow-Origin", "*",
+                            "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+                        );
+                        res.status(200).json(response);
+                    } else {
+                        getProductManager.GetProductManager(productRequest, function (error, product) {
+                            if (error != null) {
+                                response.responseHeader.status.code = 500;
+                                response.responseHeader.status.description = error;
+                                response.responsePayload.result = false;
+                                res.status(500).json(response);
+                            } else {
+                                var collection = "ProductoDetalle";
+                                var filter = { idProducto: product.id };
+                                mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
+                                    if (error != null) {
+                                        response.responseHeader.status.code = 500;
+                                        response.responseHeader.status.description = error;
+                                        response.responsePayload.result = false;
+                                        res.status(500).json(response);
                                     } else {
-                                        product.newPrice = parseInt(product.oldPrice);
-                                    }
-                                    product.categoryId = result[0].categoria;
-                                    collection = "Galeria";
-                                    mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
-                                        if (error != null) {
-                                            response.responseHeader.status.code = 500;
-                                            response.responseHeader.status.description = error;
-                                            response.responsePayload.result = false;
-                                            res.status(500).json(response);
+                                        product.ratingsValue = result[0].calificacion;
+                                        product.ratingsCount = (Math.floor(Math.random() * (5 - 1)) + 1);
+                                        product.name = result[0].informacionAdicional;
+                                        product.discount = result[0].descuento;
+                                        if (parseInt(result[0].descuento) > 0) {
+                                            product.newPrice = parseInt(product.oldPrice) - ((parseInt(result[0].descuento) * parseInt(product.oldPrice)) / 100);
                                         } else {
-                                            for (var i = 0; i < result.length; i++) {
-                                                product.images.push(
-                                                    {
-                                                        "small": result[i].imagenPequena,
-                                                        "medium": result[i].imagenMediana,
-                                                        "big": result[i].imagenGrande
-                                                    }
-                                                );
-                                            };
-                                            product.color.push(
-                                                "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "C" + (Math.floor(Math.random() * (9 - 1)) + 1) + "A" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0",
-                                                "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "B" + (Math.floor(Math.random() * (9 - 1)) + 1) + "B" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0",
-                                                "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "D" + (Math.floor(Math.random() * (9 - 1)) + 1) + "C" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0"
-                                            );
-                                            product.size.push(
-                                                "S",
-                                                "M",
-                                                "L",
-                                                "XL"
-                                            );
-                                            collection = "Calificaciones";
-                                            mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
-                                                if (error != null) {
-                                                    response.responseHeader.status.code = 500;
-                                                    response.responseHeader.status.description = error;
-                                                    response.responsePayload.result = false;
-                                                    res.status(500).json(response);    
-                                                } else {                                                    
-                                                    for (var i = 0; i < result.length; i++) {
-                                                        product.comments.push(
-                                                            {
-                                                                "comment": result[i].comentario,
-                                                                "user": "User" + (Math.floor(Math.random() * (9 - 1)) + 1) + "" + (Math.floor(Math.random() * (9 - 1)) + 1),
-                                                                "creationDate": new Date().toISOString(),
-                                                                "rating": result[i].calificacion
-                                                              }
-                                                        );
-                                                    };
-
-                                                    product.detailDescription = "Detail Description";
-                                                    product.additionalInformation = "Additional Information";
-
-                                                    response.responseHeader.status.code = 200;
-                                                    response.responseHeader.status.description = "Transacción exitosa";
-                                                    response.responsePayload.result = true;
-                                                    response.responsePayload.product = product;
-                                                    res.setHeader(
-                                                        "Access-Control-Allow-Origin", "*",
-                                                        "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
-                                                    );
-                                                    res.status(200).json(response);
-                                                }
-                                            });
+                                            product.newPrice = parseInt(product.oldPrice);
                                         }
-                                    });
-                                }
-                            });
-                        }
-                    });
+                                        product.categoryId = result[0].categoria;
+                                        collection = "Galeria";
+                                        mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
+                                            if (error != null) {
+                                                response.responseHeader.status.code = 500;
+                                                response.responseHeader.status.description = error;
+                                                response.responsePayload.result = false;
+                                                res.status(500).json(response);
+                                            } else {
+                                                for (var i = 0; i < result.length; i++) {
+                                                    product.images.push(
+                                                        {
+                                                            "small": result[i].imagenPequena,
+                                                            "medium": result[i].imagenMediana,
+                                                            "big": result[i].imagenGrande
+                                                        }
+                                                    );
+                                                };
+                                                product.color.push(
+                                                    "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "C" + (Math.floor(Math.random() * (9 - 1)) + 1) + "A" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0",
+                                                    "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "B" + (Math.floor(Math.random() * (9 - 1)) + 1) + "B" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0",
+                                                    "#" + (Math.floor(Math.random() * (9 - 1)) + 1) + "D" + (Math.floor(Math.random() * (9 - 1)) + 1) + "C" + (Math.floor(Math.random() * (9 - 1)) + 1) + "0"
+                                                );
+                                                product.size.push(
+                                                    "S",
+                                                    "M",
+                                                    "L",
+                                                    "XL"
+                                                );
+                                                collection = "Calificaciones";
+                                                mongoDB.GetCollectionFilter(collection, filter, function (error, result) {
+                                                    if (error != null) {
+                                                        response.responseHeader.status.code = 500;
+                                                        response.responseHeader.status.description = error;
+                                                        response.responsePayload.result = false;
+                                                        res.status(500).json(response);
+                                                    } else {
+                                                        for (var i = 0; i < result.length; i++) {
+                                                            product.comments.push(
+                                                                {
+                                                                    "comment": result[i].comentario,
+                                                                    "user": "User" + (Math.floor(Math.random() * (9 - 1)) + 1) + "" + (Math.floor(Math.random() * (9 - 1)) + 1),
+                                                                    "creationDate": new Date().toISOString(),
+                                                                    "rating": result[i].calificacion
+                                                                }
+                                                            );
+                                                        };
+
+                                                        product.detailDescription = "Detail Description";
+                                                        product.additionalInformation = "Additional Information";
+
+                                                        response.responseHeader.status.code = 200;
+                                                        response.responseHeader.status.description = "Transacción exitosa";
+                                                        response.responsePayload.result = true;
+                                                        response.responsePayload.product = product;
+                                                        cache.put('product' + req.body.requestPayload.id, product);
+                                                        res.setHeader(
+                                                            "Access-Control-Allow-Origin", "*",
+                                                            "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+                                                        );
+                                                        res.status(200).json(response);
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
                 }
             }
         });
