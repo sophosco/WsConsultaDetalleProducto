159d24c9efc7eaba33061133589a4c55227d8a0c
CristianDBz <CristianDBz@users-MacBook-Pro.local>
2019-03-31 15:25:34 -0500

    AB#1345 Ajuste Docker File e inclusion Test Mocha

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-let getCatalog = require('../controllers/GetCatalog');
-let getProduct = require('../controllers/GetProduct');
-let reserveProducts = require('../controllers/ReserveProducts');
+let getCatalog = require('../Controllers/GetCatalog');
+let getProduct = require('../Controllers/GetProduct');
+let reserveProducts = require('../Controllers/ReserveProducts');
 router.post('/products/catalog/', getCatalog.GetCatalog);
 router.post('/products/product/', getProduct.GetProduct);
 router.post('/products/reserve/', reserveProducts.ReserveProducts);
