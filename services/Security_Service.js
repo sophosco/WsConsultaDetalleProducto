f313a54ff891227a877736712a956797c06ef72e
fcortes326 <cortes.franck@gmail.com>
2019-04-05 15:41:32 -0500

    Se ingresan logs para la revisión de errores al conectar a mongoDB AWS

--- services/Security_Service.js
+++ services/Security_Service.js
@@ -2,16 +2,17 @@ let Request = require("request");
 let config = require('../config/Env');
 
 exports.VerifyJwtToken = function (requestVerifyJwtToken, cb) {
-    console.log("Configuracion: " + config.securityHost + " " + config.securityPort);
-    
+    console.log("Configuracion: " + config.securityHost + " " + config.securityPort);    
     Request.post({
-        "headers": { "content-type": "application/json" },
+        "headers": { "Content-Type": "application/json" },
         "url": "http://" + config.securityHost + ":" +config.securityPort + "/services/security/verifyJwtToken/",        
         "body": JSON.stringify(requestVerifyJwtToken) 
     }, (error, response, body) => {
         if (error) {
+            console.log("error: " + error); 
             cb(error, null);
         }else{
+            console.log("response.body: " + response.body);  
             cb(null, JSON.parse(response.body));
         }        
     });
