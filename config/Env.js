c77c77326e10553cc4de4127480744095140df39
Cristian Dallos <cristiandaviddallosbustos@gmail.com>
2019-04-07 00:18:19 -0500

    Se invoca API producto por HTTPs v4

--- config/Env.js
+++ config/Env.js
@@ -7,7 +7,7 @@ const env = {
     inventoryPort: process.env.POC_SERVICE_INVENTORY_PORT || "443",
     auditHost: process.env.POC_SERVICE_AUDIT_HOST || "a9b0d2398538011e9a81b0666116d3a5-1531753782.us-east-2.elb.amazonaws.com",
     auditPort: process.env.POC_SERVICE_AUDIT_PORT || "18082",
-    mongoDBHost: process.env.POC_MONGO_HOST || "mongodb",
+    mongoDBHost: process.env.POC_MONGO_HOST || "cluster01-wsnjf.mongodb.net",
     mongoDBPort: process.env.POC_MONGO_PORT || "27017",
     mongoDBUser: process.env.POC_MONGO_USER || "MongoDBUser",
     mongoDBPass: process.env.POC_MONGO_PASS || "MongoDBUser",
