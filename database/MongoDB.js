956d87bb7963a3534618c7fee33540e835d73b2f
cdallosb <cristiandaviddallosbustos@gmail.com>
2019-04-11 15:38:51 -0500

    Include HTTPs API Product

--- database/MongoDB.js
+++ database/MongoDB.js
@@ -4,31 +4,40 @@ let config = require('../config/Env');
 
 let mongoClient = require('mongodb').MongoClient;
 //let url = "mongodb+srv://"+config.mongoDBUser+":"+config.mongoDBPass+"@"+config.mongoDBHost+"/test?retryWrites=true";
-let url = "mongodb://"+config.mongoDBUser+":"+config.mongoDBPass+"@"+config.mongoDBHost+":"+config.mongoDBPort+"/"+config.mongoDB;
-//let url = "mongodb://MongoDBUser:MongoDBUser@172.20.225.242:27017/CatalogoDB";
+let url = "mongodb://" + config.mongoDBUser + ":" + config.mongoDBPass + "@" + config.mongoDBHost + ":" + config.mongoDBPort + "/" + config.mongoDB;
 let client = new mongoClient(url, { useNewUrlParser: true });
 
 exports.GetCollection = function (collection, cb) {
   console.log("Mongo: " + url);
   mongoClient.connect(url, function (err, db) {
-    if (err) cb(err, null);  
-    var dbo = db.db("CatalogoDB");
-    dbo.collection(collection).find().toArray(function (err, result) {
-      if (err) cb(err, null);     
-      cb(err, result);
-      db.close();
-    });
+    if (err) cb(err, null);
+    try {
+      var dbo = db.db("CatalogoDB");
+      if (err) cb(err, null);
+      dbo.collection(collection).find().toArray(function (err, result) {
+        if (err) cb(err, null);
+        cb(err, result);
+        db.close();
+      });
+    } catch (err) {
+      cb(err, null);
+    }
   });
 };
 
 exports.GetCollectionFilter = function (collection, filter, cb) {
   mongoClient.connect(url, function (err, db) {
-    if (err) cb(err, null);   
-    var dbo = db.db("CatalogoDB");
-    dbo.collection(collection).find(filter).toArray(function (err, result) {
-      if (err) cb(err, null);     
-      cb(err, result);
-      db.close();
-    });
+    if (err) cb(err, null);
+    try {
+      var dbo = db.db("CatalogoDB");
+      if (err) cb(err, null);
+      dbo.collection(collection).find(filter).toArray(function (err, result) {
+        if (err) cb(err, null);
+        cb(err, result);
+        db.close();
+      });
+    } catch (err) {
+      cb(err, null);
+    }
   });
 };
